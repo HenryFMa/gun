c9951e23f9fa5bdef720b340980e9ff9fae30531
Henry F Ma <henryma@edening.cn>
2019-02-09 23:00:28 +0800

    Try

--- examples/now/gun-now.js
+++ examples/now/gun-now.js
@@ -1,7 +1,12 @@
 var Gun = require('../../'); // require('gun')
 
+/*
 var server = require('http').createServer(function(req, res){
     if (Gun.serve(req, res)){ return }
 });
 
-Gun({ file: '/tmp/db/data.json', web: server.listen(), radisk: false, localStorage: true });
+Gun({ file: '/tmp/db/data.json', web: server.listen(), radisk: false, localStorage: true });*/
+
+var server = require('http').createServer().listen();
+
+Gun({web: server});
